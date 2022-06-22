b072b7f97744edf498d47c42061525a2a004537a
newenpoi <lammenschristopher119@hotmail.com>
2022-06-22 12:52:41 +0200

    model and service

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,10 @@
 from .base import db
-
 from .users import User
 from .elements import Element
+
+import config.settings as settings
+
+# Initialisation de l'ORM avec les coordonnées spécifiées...
+def setup():
+    db.bind(**settings.db_params)
+    db.generate_mapping(create_tables = True)
