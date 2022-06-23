e397665a00016fc2bf7b6021fd7673732d1f3c8c
newenpoi <lammenschristopher119@hotmail.com>
2022-06-23 17:10:54 +0200

    hotfix unix & windows path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import nextcord
 import os
 
@@ -9,6 +10,7 @@ from os import environ
 from dotenv import load_dotenv
 
 from services import element_service
+from services import interaction_service
 
 # import platform, asyncio
 # if platform.system() == 'Windows': asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -17,17 +19,16 @@ load_dotenv()
 token = environ["TOKEN"]
 if not token: exit("Aucun token n'est spécifié dans votre environnement.")
 
-# Initialisation des tables.
+# Initialisation des données.
 element_service.init()
+interaction_service.init()
 
 intents = nextcord.Intents().default(); intents.members = True; intents.message_content = True
 client = Bot(intents = intents)
 client.remove_command('help')
 
-# Loads each commands and events as extensions.
-for folder, subfolders, files in os.walk('cogs'):
-    for file in files:
-        # Désolé pour cette syntaxe qui charge les extensions des dossiers commands et events !
-        if file.endswith('py'): client.load_extension(f'{folder}.{file[:-3]}'.replace(os.sep, '.'))
+# Loads each commands and events as extensions (unix and windows path friendly).
+for f in [f'cogs.events.{f[:-3]}' for f in os.listdir((Path(__file__).parent / "cogs/events")) if f.endswith('py')]: client.load_extension(f)
+for f in [f'cogs.commands.{f[:-3]}' for f in os.listdir((Path(__file__).parent / "cogs/commands")) if f.endswith('py')]: client.load_extension(f)
 
 client.run(token)
