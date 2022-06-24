af558e0f50a654acca0fda594fc587a44276241b
newenpoi <lammenschristopher119@hotmail.com>
2022-06-24 11:10:04 +0200

    lift command

---
+++ cogs/commands/lift.py
@@ -0,0 +1,59 @@
+import random
+
+from nextcord import Embed, Interaction, Member, SlashOption, slash_command
+from nextcord.ext.commands import Bot, Cog
+from pony.orm import *
+from utils import reader
+from services import user_service
+
+# TODO : Ajuster automatiquement l'extension vers gif côté serveur (voir htaccess ou vshosts).
+
+class Lift(Cog):
+    def __init__(self, bot : Bot) -> None:
+        self.bot = bot
+
+    @slash_command(name = "soulever", description = "Soulève la personne mentionnée.", guild_ids = [535877732106764288])
+    async def lift(self, interaction: Interaction, member: Member = SlashOption(required = True, description = "Qui souhaites-tu soulever ?")) -> None:
+
+        # La réponse initiale est définie sur None.
+        response = None
+        
+        # On a soulevé moins de trois personnes dans une période d'une journée.
+        n = len(user_service.find_user_interactions_having(interaction.user.id, 'Soulever'))
+        if (n >= 3): response = await reader.read('commands/lift', 'max')
+        
+        # On a assez de vitalité pour soulever la personne.
+        health = user_service.find_user_health(interaction.user.id)
+        if (health <= 0): response = await reader.read('commands/lift', 'vitality', member.display_name)
+        
+        # La cible doit avoir assez de vitalité pour être soulevée (ne doit pas être ko).
+        health = user_service.find_user_health(member.id)
+        if (health <= 0): response = await reader.read('commands/lift', 'ko', member.display_name)
+
+        # Selon la condition de response.
+        if (response): await interaction.send(response)
+        else:
+            # On tire un nombre au hasard entre zéro et neuf.
+            health = random.randint(0, 9)
+
+            # On ajuste les points de vie de la cible.
+            user_service.update_health(member.id, -health)
+
+            # On ajoute une interaction supplémentaire.
+            user_service.add_interaction(interaction.user.id, 'Soulever')
+            
+            if (health > 0): response = await reader.read('commands/lift', 'lifted', interaction.user.display_name, member.display_name, health)
+            else: response = await reader.read('commands/lift', 'none', member.display_name)
+
+            # Embed.
+            embed = Embed(title = 'Bagarre !', description = response, color = 0xe8705a)
+            embed.set_image(url = f"http://18.168.128.213/img/hatada/lift/0{(random.randint(1, 7) if health > 0 else 0)}.gif")
+
+            await interaction.send(embed = embed)
+
+    @lift.error
+    async def error(self, interaction, error):
+        await interaction.send(f'```Stack Trace : ${error}```')
+
+def setup(bot: Bot) -> None:
+    bot.add_cog(Lift(bot))
