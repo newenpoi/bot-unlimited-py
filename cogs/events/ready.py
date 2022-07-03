8f18ccbfa6e0af40c101773504d68967d4217d85
newenpoi <lammenschristopher119@hotmail.com>
2022-07-03 12:16:22 +0200

    heuristics

--- cogs/events/ready.py
+++ cogs/events/ready.py
@@ -6,9 +6,9 @@ from nextcord import Client
 from nextcord.ext.commands import Cog
 from pony.orm import *
 
-from utils import helper
+from utils import dateutils, reader
 from utils.activity import ActivityType
-from services import user_service, interaction_service, scoreboard_service
+from services import user_service, interaction_service, scoreboard_service, binding_service
 
 class Ready(Cog):
     def __init__(self, client : Client) -> None:
@@ -50,7 +50,7 @@ class Ready(Cog):
                 interaction_service.delete_heuristic_interaction(interaction, timespan)
 
             # Wipes the scoreboard every 24 hours.
-            if (helper.temporal(datetime.now())[:-3] == '18:00'):
+            if (dateutils.temporal(datetime.now())[:-3] == '18:00'):
                 # Gets the winner of every server.
                 scores = scoreboard_service.find_top_scores()
                 
@@ -62,6 +62,30 @@ class Ready(Cog):
             
             await asyncio.sleep(timer)
     
+    async def heuristics(self, timer):
+        while True:
+            # Let's see if we have birthday to announce at this time.
+            if (dateutils.temporal(datetime.now())[:-3] == '19:00'):
+                # Find users with birthday not null for each server and date of today.
+                for guild in self.client.guilds:
+
+                    # Finds the bound channel for announcement or continues the loop if there is none.
+                    channel = binding_service.find_bound_channel(guild.id)
+                    if not channel: continue
+
+                    # All the birthdays from this server.
+                    users = user_service.find_users_having_birthday(guild.id)
+                    
+                    # Loop through all users having birthday in this server.
+                    for user in users:
+                        # Fetches the channel to announce.
+                        channel = self.client.get_channel(channel.channel)
+                        
+                        response = await reader.read('events/ready', 'birthday', user.display_name)
+                        await channel.send(response)
+            
+            await asyncio.sleep(timer)
+
     @Cog.listener()
     async def on_ready(self):
 
@@ -75,7 +99,7 @@ class Ready(Cog):
         await self.client.change_presence(status = nextcord.Status.online, activity = nextcord.Activity(name = "les échos des âmes.", type = ActivityType.listening))
 
         # Periodically syncs new users of guilds into database if there is.
-        await asyncio.gather(self.syncs(60), self.clean(60))
+        await asyncio.gather(self.syncs(60), self.clean(60), self.heuristics(60))
 
 def setup(bot: Client) -> None:
     bot.add_cog(Ready(bot))
